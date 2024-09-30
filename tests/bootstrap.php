b206b7397661cdb99127e9ad233f161c326a82fa
Jan Vaníček <jan.vanicek@keboola.com>
2024-09-30 15:57:21 +0200

    feat(job-configuration): Load local envs when bootstrapping tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,4 +2,19 @@
 
 declare(strict_types=1);
 
+use Symfony\Component\Dotenv\Dotenv;
+
 require __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = (new Dotenv())->usePutenv();
+
+$envLocalPath = dirname(__DIR__) . '/.env.local';
+if (!file_exists($envLocalPath)) {
+    throw new RuntimeException('.env.local file not found.');
+}
+
+$dotenv->bootEnv(
+    path: $envLocalPath,
+    testEnvs: [],
+    overrideExistingVars: true
+);
