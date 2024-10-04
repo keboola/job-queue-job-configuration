ca8db2cd7ea31beb57754253f9a8d03b9884e5bb
Jan Vaníček <jan.vanicek@keboola.com>
2024-10-04 17:13:05 +0200

    tests(job-configuration): DataLoader tests migration: create from backend-specific-tests/ABS dir
    See https://github.com/keboola/docker-bundle/blob/9c5ac077889c0e4fe64d4e488acc397711f40742/Tests/backend-specific-tests/ABS/DataLoaderABSTest.php and https://github.com/keboola/docker-bundle/blob/7f4329e7e69c95019ab201c82e7d5ab47e5bb06b/Tests/backend-specific-tests/ABS/DataLoaderPersistentABSWorkspaceTest.php.

---
+++ tests/Mapping/InputDataLoader/ABSInputDataLoaderTest.php
@@ -0,0 +1,112 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\JobQueue\JobConfiguration\Tests\Mapping\InputDataLoader;
+
+use Keboola\Csv\CsvFile;
+use Keboola\JobQueue\JobConfiguration\JobDefinition\Configuration\Configuration;
+use Keboola\JobQueue\JobConfiguration\JobDefinition\Configuration\Storage\FilesList;
+use Keboola\JobQueue\JobConfiguration\JobDefinition\Configuration\Storage\Input;
+use Keboola\JobQueue\JobConfiguration\JobDefinition\Configuration\Storage\Storage;
+use Keboola\JobQueue\JobConfiguration\JobDefinition\Configuration\Storage\TablesList;
+use Keboola\JobQueue\JobConfiguration\JobDefinition\State\State;
+use Keboola\StorageApi\Options\FileUploadOptions;
+use SplFileInfo;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
+
+class ABSInputDataLoaderTest extends BaseInputDataLoaderTest
+{
+    protected const DEFAULT_PROJECT = 'azure';
+
+    public function testLoadInputData(): void
+    {
+        $storage = new Storage(
+            input: new Input(
+                tables: new TablesList([
+                    [
+                        'source' => 'in.c-docker-demo-testConfig-abs.test',
+                    ],
+                ]),
+                files: new FilesList([
+                    ['tags' => ['docker-demo-test-abs'], 'overwrite' => true],
+                ]),
+            ),
+        );
+        $fs = new Filesystem();
+        $filePath = $this->getDataDirPath() . '/upload/tables/test.csv';
+        $fs->dumpFile(
+            $filePath,
+            "id,text,row_number\n1,test,1\n1,test,2\n1,test,3",
+        );
+
+        $this->clientWrapper->getBasicClient()->createBucket('docker-demo-testConfig-abs', 'in');
+        $this->clientWrapper->getBasicClient()->createTable(
+            'in.c-docker-demo-testConfig-abs',
+            'test',
+            new CsvFile($filePath),
+        );
+        $this->clientWrapper->getBasicClient()->uploadFile(
+            $filePath,
+            (new FileUploadOptions())->setTags(['docker-demo-test-abs']),
+        );
+        sleep(1);
+
+        $dataLoader = $this->getInputDataLoader();
+        $dataLoader->loadInputData(
+            component: $this->getComponent(),
+            jobConfiguration: new Configuration(
+                storage: $storage,
+            ),
+            jobState: new State(),
+        );
+
+        $manifest = json_decode(
+            // @phpstan-ignore-next-line
+            file_get_contents(
+                $this->getDataDirPath() . '/in/tables/in.c-docker-demo-testConfig-abs.test.manifest',
+            ),
+            true,
+        );
+
+        $finder = new Finder();
+        $finder->files()->in($this->getDataDirPath() . '/in/files')->notName('*.manifest');
+
+        $this->assertEquals(1, $finder->count());
+
+        /** @var SplFileInfo $file */
+        foreach ($finder as $file) {
+            $this->assertEquals(
+                "id,text,row_number\n1,test,1\n1,test,2\n1,test,3",
+                file_get_contents($file->getPathname()),
+            );
+
+            $fileManifest = json_decode(
+                // @phpstan-ignore-next-line
+                file_get_contents($file->getPathname() . '.manifest'),
+                true,
+            );
+
+            self::assertIsArray($fileManifest);
+            self::assertArrayHasKey('id', $fileManifest);
+            self::assertArrayHasKey('name', $fileManifest);
+            self::assertArrayHasKey('created', $fileManifest);
+            self::assertArrayHasKey('is_public', $fileManifest);
+            self::assertArrayHasKey('is_encrypted', $fileManifest);
+            self::assertArrayHasKey('tags', $fileManifest);
+            self::assertArrayHasKey('max_age_days', $fileManifest);
+            self::assertArrayHasKey('size_bytes', $fileManifest);
+            self::assertArrayHasKey('is_sliced', $fileManifest);
+        }
+
+        self::assertIsArray($manifest);
+        $this->assertArrayHasKey('id', $manifest);
+        $this->assertArrayHasKey('name', $manifest);
+        $this->assertArrayHasKey('created', $manifest);
+        $this->assertArrayHasKey('uri', $manifest);
+        $this->assertArrayHasKey('primary_key', $manifest);
+        $this->assertEquals('in.c-docker-demo-testConfig-abs.test', $manifest['id']);
+        $this->assertEquals('test', $manifest['name']);
+    }
+}
---
+++ tests/Mapping/OutputDataLoader/ABSPersistentOutputDataLoaderTest.php
@@ -0,0 +1,363 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\JobQueue\JobConfiguration\Tests\Mapping\OutputDataLoader;
+
+use ColinODell\PsrTestLogger\TestLogger;
+use Keboola\InputMapping\Staging\AbstractStrategyFactory;
+use Keboola\JobQueue\JobConfiguration\Exception\ApplicationException;
+use Keboola\JobQueue\JobConfiguration\JobDefinition\Component\ComponentSpecification;
+use Keboola\JobQueue\JobConfiguration\JobDefinition\Configuration\Configuration as JobConfiguration;
+use Keboola\JobQueue\JobConfiguration\JobDefinition\Configuration\Storage\Storage;
+use Keboola\JobQueue\JobConfiguration\Mapping\WorkspaceProviderFactoryFactory;
+use Keboola\StorageApi\BranchAwareClient;
+use Keboola\StorageApi\Components;
+use Keboola\StorageApi\Options\Components\Configuration;
+use Keboola\StorageApi\Options\Components\ListConfigurationWorkspacesOptions;
+use Keboola\StorageApi\Workspaces;
+use Keboola\StorageApiBranch\ClientWrapper;
+use Keboola\StorageApiBranch\Factory\ClientOptions;
+use Keboola\StorageApiBranch\StorageApiToken;
+
+class ABSPersistentOutputDataLoaderTest extends BaseOutputDataLoaderTest
+{
+    protected const COMPONENT_ID = 'keboola.runner-config-test';
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->clientWrapper = new ClientWrapper(
+            new ClientOptions(
+                (string) getenv('STORAGE_API_URL_ABS'),
+                (string) getenv('TEST_STORAGE_API_TOKEN_MASTER_ABS'),
+            ),
+        );
+    }
+
+    public function testAbsWorkspaceNoConfig(): void
+    {
+        $component = new ComponentSpecification([
+            'id' => 'docker-demo',
+            'data' => [
+                'definition' => [
+                    'type' => 'dockerhub',
+                    'uri' => 'keboola/docker-demo',
+                    'tag' => 'master',
+                ],
+                'staging-storage' => [
+                    'input' => 'workspace-abs',
+                    'output' => 'workspace-abs',
+                ],
+            ],
+        ]);
+        $client = $this->getMockBuilder(BranchAwareClient::class)
+            ->setConstructorArgs([
+                'default',
+                [
+                    'url' => getenv('STORAGE_API_URL_AZURE'),
+                    'token' => getenv('TEST_STORAGE_API_TOKEN_MASTER_AZURE'),
+                ],
+            ])
+            ->onlyMethods(['apiDelete'])
+            ->getMock();
+        $client->expects($this->once())->method('apiDelete')->with(self::stringContains('workspaces/'));
+        $clientWrapper = $this->createMock(ClientWrapper::class);
+        $clientWrapper->method('getBasicClient')->willReturn($client);
+        $clientWrapper->method('getTableAndFileStorageClient')->willReturn($client);
+        $clientWrapper->method('getBranchClient')->willReturn($client);
+        $clientWrapper->method('getToken')->willReturn(new StorageApiToken(
+            tokenInfo: ['owner' => ['fileStorageProvider' => 'azure', 'features' => []]],
+            tokenValue: 'token',
+        ));
+        $logger = new TestLogger();
+
+        $dataLoader = $this->getOutputDataLoader(
+            clientWrapper: $clientWrapper,
+            logger: $logger,
+            componentStagingStorageType: AbstractStrategyFactory::WORKSPACE_ABS,
+        );
+        $dataLoader->storeOutput(
+            component: $component,
+            jobConfiguration: new JobConfiguration(
+                storage: new Storage(),
+            ),
+            branchId: null,
+            runId: null,
+            configId: null,
+            configRowId: null,
+            projectFeatures: [],
+        );
+
+        $credentials = $dataLoader->getWorkspaceCredentials();
+        self::assertEquals(['connectionString', 'container'], array_keys($credentials));
+        self::assertStringStartsWith('BlobEndpoint=https://', $credentials['connectionString']);
+        self::assertTrue($logger->hasNoticeThatContains('Created a new ephemeral workspace.'));
+        $dataLoader->cleanWorkspace($component);
+        // checked in mock that the workspace is deleted
+    }
+
+    public function testAbsWorkspaceConfigNoWorkspace(): void
+    {
+        $component = new ComponentSpecification([
+            'id' => 'keboola.runner-config-test',
+            'data' => [
+                'definition' => [
+                    'type' => 'dockerhub',
+                    'uri' => 'keboola/docker-demo',
+                    'tag' => 'master',
+                ],
+                'staging-storage' => [
+                    'input' => 'workspace-abs',
+                    'output' => 'workspace-abs',
+                ],
+            ],
+        ]);
+        $client = $this->getMockBuilder(BranchAwareClient::class)
+            ->setConstructorArgs([
+                'default',
+                [
+                    'url' => getenv('STORAGE_API_URL_AZURE'),
+                    'token' => getenv('TEST_STORAGE_API_TOKEN_MASTER_AZURE'),
+                ],
+            ])
+            ->onlyMethods(['apiDelete'])
+            ->getMock();
+        $client->expects(self::never())->method('apiDelete');
+
+        $configuration = new Configuration();
+        $configuration->setConfiguration([]);
+        $configuration->setName('test-dataloader');
+        $configuration->setComponentId('keboola.runner-config-test');
+
+        $componentsApi = new Components($this->clientWrapper->getBasicClient());
+        $configurationId = $componentsApi->addConfiguration($configuration)['id'];
+        $clientWrapper = $this->createMock(ClientWrapper::class);
+        $clientWrapper->method('getBasicClient')->willReturn($client);
+        $clientWrapper->method('getTableAndFileStorageClient')->willReturn($client);
+        $clientWrapper->method('getBranchClient')->willReturn($client);
+        $clientWrapper->method('getToken')->willReturn(new StorageApiToken(
+            ['owner' => ['fileStorageProvider' => 'azure', 'features' => []]],
+            'token',
+        ));
+
+        $logger = new TestLogger();
+        $dataLoader = $this->getOutputDataLoader(
+            clientWrapper: $clientWrapper,
+            logger: $logger,
+            configId: $configurationId,
+            componentStagingStorageType: AbstractStrategyFactory::WORKSPACE_ABS,
+        );
+        $dataLoader->storeOutput(
+            component: $component,
+            jobConfiguration: new JobConfiguration(
+                storage: new Storage(),
+            ),
+            branchId: null,
+            runId: null,
+            configId: $configurationId,
+            configRowId: null,
+            projectFeatures: [],
+        );
+
+        $credentials = $dataLoader->getWorkspaceCredentials();
+        self::assertEquals(['connectionString', 'container'], array_keys($credentials));
+        self::assertStringStartsWith('BlobEndpoint=https://', $credentials['connectionString']);
+
+        $workspaces = $componentsApi->listConfigurationWorkspaces(
+            (new ListConfigurationWorkspacesOptions())
+                ->setComponentId('keboola.runner-config-test')
+                ->setConfigurationId($configurationId),
+        );
+        self::assertCount(1, $workspaces);
+        $dataLoader->cleanWorkspace($component, $configurationId);
+        // double check that workspace still exists
+        $workspaces = $componentsApi->listConfigurationWorkspaces(
+            (new ListConfigurationWorkspacesOptions())
+                ->setComponentId('keboola.runner-config-test')
+                ->setConfigurationId($configurationId),
+        );
+        self::assertCount(1, $workspaces);
+        // cleanup after the test
+        $workspacesApi = new Workspaces($this->clientWrapper->getBasicClient());
+        $workspacesApi->deleteWorkspace($workspaces[0]['id'], [], true);
+        self::assertTrue($logger->hasInfoThatContains('Created a new persistent workspace'));
+    }
+
+    public function testAbsWorkspaceConfigOneWorkspace(): void
+    {
+        $component = new ComponentSpecification([
+            'id' => 'keboola.runner-config-test',
+            'data' => [
+                'definition' => [
+                    'type' => 'dockerhub',
+                    'uri' => 'keboola/docker-demo',
+                    'tag' => 'master',
+                ],
+                'staging-storage' => [
+                    'input' => 'workspace-abs',
+                    'output' => 'workspace-abs',
+                ],
+            ],
+        ]);
+        $client = $this->getMockBuilder(BranchAwareClient::class)
+            ->setConstructorArgs([
+                'default',
+                [
+                    'url' => getenv('STORAGE_API_URL_AZURE'),
+                    'token' => getenv('TEST_STORAGE_API_TOKEN_MASTER_AZURE'),
+                ],
+            ])
+            ->onlyMethods(['apiDelete'])
+            ->getMock();
+        $client->expects(self::never())->method('apiDelete');
+        $configuration = new Configuration();
+        $configuration->setConfiguration([]);
+        $configuration->setName('test-dataloader');
+        $configuration->setComponentId('keboola.runner-config-test');
+        $componentsApi = new Components($client);
+        $configurationId = $componentsApi->addConfiguration($configuration)['id'];
+
+        $workspace = $componentsApi->createConfigurationWorkspace(
+            'keboola.runner-config-test',
+            $configurationId,
+            ['backend' => 'abs'],
+            true,
+        );
+        $clientWrapper = $this->createMock(ClientWrapper::class);
+        $clientWrapper->method('getBasicClient')->willReturn($client);
+        $clientWrapper->method('getTableAndFileStorageClient')->willReturn($client);
+        $clientWrapper->method('getBranchClient')->willReturn($client);
+        $clientWrapper->method('getToken')->willReturn(new StorageApiToken(
+            tokenInfo: ['owner' => ['fileStorageProvider' => 'azure', 'features' => []]],
+            tokenValue: 'token',
+        ));
+        $logger = new TestLogger();
+
+        $dataLoader = $this->getOutputDataLoader(
+            clientWrapper: $clientWrapper,
+            logger: $logger,
+            configId: $configurationId,
+            componentStagingStorageType: AbstractStrategyFactory::WORKSPACE_ABS,
+        );
+        $dataLoader->storeOutput(
+            component: $component,
+            jobConfiguration: new JobConfiguration(
+                storage: new Storage(),
+            ),
+            branchId: null,
+            runId: null,
+            configId: $configurationId,
+            configRowId: null,
+            projectFeatures: [],
+        );
+
+        $credentials = $dataLoader->getWorkspaceCredentials();
+        self::assertEquals(['connectionString', 'container'], array_keys($credentials));
+        self::assertStringStartsWith('BlobEndpoint=https://', $credentials['connectionString']);
+        $workspaces = $componentsApi->listConfigurationWorkspaces(
+            (new ListConfigurationWorkspacesOptions())
+                ->setComponentId('keboola.runner-config-test')
+                ->setConfigurationId($configurationId),
+        );
+        self::assertCount(1, $workspaces);
+        $dataLoader->cleanWorkspace($component, $configurationId);
+        // double check that workspace still exists
+        $workspaces = $componentsApi->listConfigurationWorkspaces(
+            (new ListConfigurationWorkspacesOptions())
+                ->setComponentId('keboola.runner-config-test')
+                ->setConfigurationId($configurationId),
+        );
+        self::assertCount(1, $workspaces);
+        // and it must be the same workspace we created beforehand
+        self::assertEquals($workspace['id'], $workspaces[0]['id']);
+        // cleanup after the test
+        $workspacesApi = new Workspaces($this->clientWrapper->getBasicClient());
+        $workspacesApi->deleteWorkspace($workspaces[0]['id'], [], true);
+        self::assertTrue($logger->hasInfoThatContains(
+            sprintf('Reusing persistent workspace "%s".', $workspace['id']),
+        ));
+    }
+
+    public function testAbsWorkspaceConfigMultipleWorkspace(): void
+    {
+        $component = new ComponentSpecification([
+            'id' => 'keboola.runner-config-test',
+            'data' => [
+                'definition' => [
+                    'type' => 'dockerhub',
+                    'uri' => 'keboola/docker-demo',
+                    'tag' => 'master',
+                ],
+                'staging-storage' => [
+                    'input' => 'workspace-abs',
+                    'output' => 'workspace-abs',
+                ],
+            ],
+        ]);
+        $client = $this->getMockBuilder(BranchAwareClient::class)
+            ->setConstructorArgs([
+                'default',
+                [
+                    'url' => getenv('STORAGE_API_URL_AZURE'),
+                    'token' => getenv('TEST_STORAGE_API_TOKEN_MASTER_AZURE'),
+                ],
+            ])
+            ->onlyMethods(['apiDelete'])
+            ->getMock();
+        $client->expects(self::never())->method('apiDelete');
+
+        $configuration = new Configuration();
+        $configuration->setConfiguration([]);
+        $configuration->setName('test-dataloader');
+        $configuration->setComponentId('keboola.runner-config-test');
+        $componentsApi = new Components($this->clientWrapper->getBasicClient());
+        $configurationId = $componentsApi->addConfiguration($configuration)['id'];
+        $workspace1 = $componentsApi->createConfigurationWorkspace(
+            'keboola.runner-config-test',
+            $configurationId,
+            ['backend' => 'abs'],
+            true,
+        );
+        $workspace2 = $componentsApi->createConfigurationWorkspace(
+            'keboola.runner-config-test',
+            $configurationId,
+            ['backend' => 'abs'],
+            true,
+        );
+
+        $clientWrapper = $this->createMock(ClientWrapper::class);
+        $clientWrapper->method('getBasicClient')->willReturn($client);
+        $clientWrapper->method('getBranchClient')->willReturn($client);
+        $logger = new TestLogger();
+        try {
+            $workspaceFactory = new WorkspaceProviderFactoryFactory(
+                new Components($clientWrapper->getBranchClient()),
+                new Workspaces($clientWrapper->getBranchClient()),
+                $logger,
+            );
+            $workspaceFactory->getWorkspaceProviderFactory(
+                'workspace-abs',
+                $component,
+                $configurationId,
+                null,
+                null,
+            );
+        } catch (ApplicationException $e) {
+            self::assertEquals(
+                sprintf(
+                    'Multiple workspaces (total 2) found (IDs: %s, %s) for configuration "%s" of component "%s".',
+                    $workspace1['id'],
+                    $workspace2['id'],
+                    $configurationId,
+                    'keboola.runner-config-test',
+                ),
+                $e->getMessage(),
+            );
+            $workspacesApi = new Workspaces($this->clientWrapper->getBasicClient());
+            $workspacesApi->deleteWorkspace($workspace1['id'], [], true);
+            $workspacesApi->deleteWorkspace($workspace2['id'], [], true);
+        }
+    }
+}
