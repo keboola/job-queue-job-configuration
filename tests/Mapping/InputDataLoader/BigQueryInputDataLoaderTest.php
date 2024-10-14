7883864b8aa1923c98be3240330137ae40f2cf11
Jan Vaníček <jan.vanicek@keboola.com>
2024-10-14 19:19:05 +0200

    tests(job-configuration): DataLoader tests migration: create from backend-specific-tests/BigQuery dir
    See https://github.com/keboola/docker-bundle/blob/ba7fe1eeb3ad78ac690d2d7768df80ec6386d72f/Tests/backend-specific-tests/BigQuery/DataLoaderBigQueryTest.php.

---
+++ tests/Mapping/InputDataLoader/BigQueryInputDataLoaderTest.php
@@ -0,0 +1,188 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\JobQueue\JobConfiguration\Tests\Mapping\InputDataLoader;
+
+use Keboola\Csv\CsvFile;
+use Keboola\InputMapping\Staging\AbstractStrategyFactory;
+use Keboola\JobQueue\JobConfiguration\JobDefinition\Component\ComponentSpecification;
+use Keboola\JobQueue\JobConfiguration\JobDefinition\Configuration\Configuration as JobConfiguration;
+use Keboola\JobQueue\JobConfiguration\JobDefinition\Configuration\Storage\Input;
+use Keboola\JobQueue\JobConfiguration\JobDefinition\Configuration\Storage\Storage;
+use Keboola\JobQueue\JobConfiguration\JobDefinition\Configuration\Storage\TablesList;
+use Keboola\JobQueue\JobConfiguration\JobDefinition\State\State;
+use Keboola\StorageApi\ClientException;
+use Keboola\StorageApi\Components;
+use Keboola\StorageApi\Options\Components\Configuration;
+use Keboola\StorageApi\Options\Components\ListConfigurationWorkspacesOptions;
+use Keboola\StorageApi\Workspaces;
+use Keboola\StorageApiBranch\ClientWrapper;
+use Keboola\StorageApiBranch\Factory\ClientOptions;
+use Symfony\Component\Filesystem\Filesystem;
+
+class BigQueryInputDataLoaderTest extends BaseInputDataLoaderTest
+{
+    protected const COMPONENT_ID = 'keboola.runner-workspace-bigquery-test';
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->cleanupBucketAndFiles();
+    }
+
+    public function testWorkspaceBigQueryNoPreserve(): void
+    {
+        $this->clientWrapper = new ClientWrapper(
+            new ClientOptions(
+                (string) getenv('STORAGE_API_URL_BIGQUERY'),
+                (string) getenv('TEST_STORAGE_API_TOKEN_BIGQUERY'),
+            ),
+        );
+
+        $bucketName = 'testWorkspaceBigQueryNoPreserve';
+        try {
+            $buckets = $this->clientWrapper->getBasicClient()->listBuckets();
+            foreach ($buckets as $bucket) {
+                if ($bucket['stage'] === 'in' && $bucket['displayName'] === $bucketName) {
+                    $this->clientWrapper->getBasicClient()->dropBucket(
+                        $bucket['id'],
+                        ['force' => true, 'async' => true],
+                    );
+                }
+            }
+        } catch (ClientException $e) {
+            if ($e->getCode() !== 404) {
+                throw $e;
+            }
+        }
+
+        $bucketId = $this->clientWrapper->getBasicClient()->createBucket(
+            $bucketName,
+            'in',
+            'description',
+            'bigquery',
+        );
+        $fs = new Filesystem();
+        $fs->dumpFile(
+            $this->getTmpDirPath() . '/data.csv',
+            "id,text,row_number\n1,test,1\n1,test,2\n1,test,3",
+        );
+        $csv = new CsvFile($this->getTmpDirPath() . '/data.csv');
+        $this->clientWrapper->getBasicClient()->createTableAsync($bucketId, 'test', $csv);
+
+        $component = new ComponentSpecification([
+            'id' => self::COMPONENT_ID,
+            'data' => [
+                'definition' => [
+                    'type' => 'aws-ecr',
+                    // phpcs:ignore Generic.Files.LineLength.MaxExceeded
+                    'uri' => '147946154733.dkr.ecr.us-east-1.amazonaws.com/developer-portal-v2/keboola.runner-workspace-test',
+                    'tag' => '1.7.1',
+                ],
+                'staging-storage' => [
+                    'input' => 'workspace-bigquery',
+                    'output' => 'workspace-bigquery',
+                ],
+            ],
+        ]);
+
+        $storage = new Storage(
+            input: new Input(
+                tables: new TablesList([
+                    [
+                        'source' => sprintf('%s.test', $bucketId),
+                        'destination' => 'test',
+                        'keep_internal_timestamp_column' => false,
+                    ],
+                ]),
+            ),
+        );
+        $configuration = new Configuration();
+        $configuration->setName('testWorkspaceBigQueryPreserve');
+        $configuration->setComponentId(self::COMPONENT_ID);
+        // $configuration->setConfiguration($config);
+        $componentsApi = new Components($this->clientWrapper->getBasicClient());
+        $configId = $componentsApi->addConfiguration($configuration)['id'];
+
+        // create bigquery workspace and load a table into it
+        $workspace = $componentsApi->createConfigurationWorkspace(
+            self::COMPONENT_ID,
+            $configId,
+            ['backend' => 'bigquery'],
+            true,
+        );
+        $workspaceApi = new Workspaces($this->clientWrapper->getBasicClient());
+        $workspaceApi->loadWorkspaceData(
+            $workspace['id'],
+            [
+                'input' => [
+                    [
+                        'source' => sprintf('%s.test', $bucketId),
+                        'destination' => 'original',
+                        'useView' => true,
+                    ],
+                ],
+            ],
+        );
+
+        $dataLoader = $this->getInputDataLoader(
+            clientWrapper: $this->clientWrapper,
+            configId: $configId,
+            componentStagingStorageType: AbstractStrategyFactory::WORKSPACE_BIGQUERY,
+        );
+        $dataLoader->loadInputData(
+            component: $component,
+            jobConfiguration: new JobConfiguration(
+                storage: $storage,
+            ),
+            jobState: new State(),
+        );
+        $credentials = $dataLoader->getWorkspaceCredentials();
+        self::assertEquals(['schema', 'region', 'credentials'], array_keys($credentials));
+        self::assertNotEmpty($credentials['credentials']);
+
+        $workspaces = $componentsApi->listConfigurationWorkspaces(
+            (new ListConfigurationWorkspacesOptions())
+                ->setComponentId(self::COMPONENT_ID)
+                ->setConfigurationId($configId),
+        );
+        // workspace is not reused so another one was created
+        self::assertCount(2, $workspaces);
+
+        // the workspace is not reused and not the same
+        self::assertNotSame($workspace['connection']['schema'], $credentials['schema']);
+
+        // but the original table does exists (workspace was not cleared)
+        try {
+            $this->clientWrapper->getBasicClient()->writeTableAsyncDirect(
+                sprintf('%s.test', $bucketId),
+                ['dataWorkspaceId' => $workspaces[0]['id'], 'dataTableName' => 'original'],
+            );
+            self::fail('Must throw exception');
+        } catch (ClientException $e) {
+            self::assertStringContainsString(
+            // phpcs:ignore Generic.Files.LineLength.MaxExceeded
+                'Invalid columns: _timestamp: Only alphanumeric characters and underscores are allowed in column name. Underscore is not allowed on the beginning',
+                $e->getMessage(),
+            );
+        }
+
+        try {
+            // the loaded table exists, but can not be loaded because of _timestamp column
+            $this->clientWrapper->getBasicClient()->writeTableAsyncDirect(
+                sprintf('%s.test', $bucketId),
+                ['dataWorkspaceId' => $workspaces[0]['id'], 'dataTableName' => 'original'],
+            );
+            self::fail('Must throw exception');
+        } catch (ClientException $e) {
+            self::assertStringContainsString(
+            // phpcs:ignore Generic.Files.LineLength.MaxExceeded
+                'Invalid columns: _timestamp: Only alphanumeric characters and underscores are allowed in column name. Underscore is not allowed on the beginning',
+                $e->getMessage(),
+            );
+        }
+
+        $workspaceApi->deleteWorkspace($workspace['id'], async: true);
+    }
+}
