f53b0fc0fde0cc3358b97944ce3ff27a9734e60c
Roman Pištěk <roman.pistek@keboola.com>
2025-02-27 21:23:50 +0100

    feat: Add JobDefinition::$configVersion

--- src/JobDefinition/JobDefinition.php
+++ src/JobDefinition/JobDefinition.php
@@ -4,18 +4,21 @@ declare(strict_types=1);
 
 namespace Keboola\JobQueue\JobConfiguration\JobDefinition;
 
-use Keboola\JobQueue\JobConfiguration\JobDefinition\Configuration;
-use Keboola\JobQueue\JobConfiguration\JobDefinition\State;
+use InvalidArgumentException;
 
 readonly class JobDefinition
 {
     public function __construct(
         public Component\ComponentSpecification $component,
         public ?string $configId,
+        public ?string $configVersion,
         public ?string $rowId,
         public bool $isDisabled,
         public Configuration\Configuration $configuration,
         public State\State $state,
     ) {
+        if (empty($this->configId) !== empty($this->configVersion)) {
+            throw new InvalidArgumentException('configId and configVersion must either both be set or both left empty.');
+        }
     }
 }
