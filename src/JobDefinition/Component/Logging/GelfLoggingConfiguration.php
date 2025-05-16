b5503dd0d81f166cf82cf0a8c98b5abd3457263d
Pepa Martinec <josef.martinec@keboola.com>
2025-05-16 17:47:15 +0100

    fix: There is no port configuration for Gelf logger

--- src/JobDefinition/Component/Logging/GelfLoggingConfiguration.php
+++ src/JobDefinition/Component/Logging/GelfLoggingConfiguration.php
@@ -6,14 +6,10 @@ namespace Keboola\JobQueue\JobConfiguration\JobDefinition\Component\Logging;
 
 class GelfLoggingConfiguration implements LoggingConfigurationInterface
 {
-    /** @var non-empty-string */
-    public readonly string $port;
-
     public function __construct(
         /** @var non-empty-string */
         public readonly string $type,
     ) {
-        $this->port = $this->type === 'http' ? '12002' : '12001';
     }
 
     public static function fromArray(array $data): self
