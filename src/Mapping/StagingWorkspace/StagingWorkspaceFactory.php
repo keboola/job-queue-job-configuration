eb92380a2cc0bc6ed35b20789657c3ba295da5cc
Pepa Martinec <josef.martinec@keboola.com>
2025-05-23 16:58:50 +0100

    chore[job-configuration]: Polish code

--- src/Mapping/StagingWorkspace/StagingWorkspaceFactory.php
+++ src/Mapping/StagingWorkspace/StagingWorkspaceFactory.php
@@ -7,6 +7,7 @@ namespace Keboola\JobQueue\JobConfiguration\Mapping\StagingWorkspace;
 use Keboola\JobQueue\JobConfiguration\Exception\ApplicationException;
 use Keboola\JobQueue\JobConfiguration\JobDefinition\Component\ComponentSpecification;
 use Keboola\JobQueue\JobConfiguration\JobDefinition\Configuration\Configuration;
+use Keboola\JobQueue\JobConfiguration\JobDefinition\Configuration\Runtime\WorkspaceCredentials;
 use Keboola\StagingProvider\Staging\StagingClass;
 use Keboola\StagingProvider\Staging\StagingType;
 use Keboola\StagingProvider\Workspace\Configuration\NetworkPolicy;
@@ -35,38 +36,82 @@ class StagingWorkspaceFactory
             return null;
         }
 
+        $externallyManagedWorkspaceCredentials = $configuration->runtime?->backend?->workspaceCredentials;
+        if ($externallyManagedWorkspaceCredentials) {
+            return $this->getExistingWorkspaceStagingFacade(
+                $externallyManagedWorkspaceCredentials,
+            );
+        } else {
+            return $this->getNewWorkspaceStagingFacade(
+                $stagingType,
+                $component,
+                $configuration,
+                $configId,
+                $storageApiToken,
+            );
+        }
+    }
+
+    private function resolveStagingForComponent(ComponentSpecification $component): StagingType
+    {
+        $inputStagingType = StagingType::from($component->getInputStagingStorage());
+        $outputStagingType = StagingType::from($component->getOutputStagingStorage());
+
+        if ($inputStagingType->getStagingClass() === StagingClass::Workspace &&
+            $outputStagingType->getStagingClass() === StagingClass::Workspace &&
+            $inputStagingType !== $outputStagingType
+        ) {
+            throw new ApplicationException(sprintf(
+                'Component staging setting mismatch - input: "%s", output: "%s".',
+                $inputStagingType->value,
+                $outputStagingType->value,
+            ));
+        }
+
+        // now we're sure that input and output staging type are the same for workspace staging
+        return $inputStagingType;
+    }
+
+    private function getExistingWorkspaceStagingFacade(
+        WorkspaceCredentials $externallyManagedWorkspaceCredentials,
+    ): StagingWorkspaceFacade {
+        $this->logger->notice(sprintf(
+            'Using provided workspace "%s".',
+            $externallyManagedWorkspaceCredentials->id,
+        ));
+
+        $workspace = $this->workspaceProvider->getExistingWorkspace(
+            $externallyManagedWorkspaceCredentials->id,
+            $externallyManagedWorkspaceCredentials->getCredentials(),
+        );
+
+        return new StagingWorkspaceFacade(
+            $this->workspaceProvider,
+            $this->logger,
+            $workspace,
+            isReusable: true, // Externally managed workspaces are persistent
+        );
+    }
+
+    private function getNewWorkspaceStagingFacade(
+        StagingType $stagingType,
+        ComponentSpecification $component,
+        Configuration $configuration,
+        ?string $configId,
+        StorageApiToken $storageApiToken
+    ): StagingWorkspaceFacade {
         $backendConfig = $configuration->runtime?->backend;
         $useReadonlyRole = $configuration->storage->input->readOnlyStorageAccess;
-
-        $externallyManagedWorkspaceCredentials = $backendConfig?->workspaceCredentials;
-        if ($externallyManagedWorkspaceCredentials) {
-            $this->logger->notice(sprintf(
-                'Using provided workspace "%s".',
-                $externallyManagedWorkspaceCredentials->id,
-            ));
-
-            $workspace = $this->workspaceProvider->getExistingWorkspace(
-                $externallyManagedWorkspaceCredentials->id,
-                $externallyManagedWorkspaceCredentials->getCredentials(),
-            );
-
-            return new StagingWorkspaceFacade(
-                $this->workspaceProvider,
-                $this->logger,
-                $workspace,
-                isReusable: true, // Externally managed workspaces are persistent
-            );
-        }
-
-        $workspaceLoginType = null;
-        if ($stagingType === StagingType::WorkspaceSnowflake && $component->useSnowflakeKeyPairAuth()) {
-            $workspaceLoginType = WorkspaceLoginType::SNOWFLAKE_SERVICE_KEYPAIR;
-        }
 
         $this->logger->notice(sprintf(
             'Creating a new %s workspace.',
             $useReadonlyRole ? 'readonly ephemeral' : 'ephemeral',
         ));
+
+        $workspaceLoginType = null;
+        if ($stagingType === StagingType::WorkspaceSnowflake && $component->useSnowflakeKeyPairAuth()) {
+            $workspaceLoginType = WorkspaceLoginType::SNOWFLAKE_SERVICE_KEYPAIR;
+        }
 
         $workspaceConfig = new NewWorkspaceConfig(
             stagingType: $stagingType,
@@ -89,25 +134,5 @@ class StagingWorkspaceFactory
             $workspace,
             isReusable: false,
         );
-    }
-
-    private function resolveStagingForComponent(ComponentSpecification $component): StagingType
-    {
-        $inputStagingType = StagingType::from($component->getInputStagingStorage());
-        $outputStagingType = StagingType::from($component->getOutputStagingStorage());
-
-        if ($inputStagingType->getStagingClass() === StagingClass::Workspace &&
-            $outputStagingType->getStagingClass() === StagingClass::Workspace &&
-            $inputStagingType !== $outputStagingType
-        ) {
-            throw new ApplicationException(sprintf(
-                'Component staging setting mismatch - input: "%s", output: "%s".',
-                $inputStagingType->value,
-                $outputStagingType->value,
-            ));
-        }
-
-        // now we're sure that input and output staging type are the same for workspace staging
-        return $inputStagingType;
     }
 }
