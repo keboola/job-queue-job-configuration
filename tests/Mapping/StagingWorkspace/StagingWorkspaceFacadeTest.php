99421ecccb171e76336346fd332da2807350615e
Pepa Martinec <josef.martinec@keboola.com>
2025-05-26 11:40:16 +0100

    test[job-configuration]: Fix StagingWorkspace tests placement

--- tests/Mapping/StagingWorkspace/StagingWorkspaceFacadeTest.php
+++ tests/Mapping/StagingWorkspace/StagingWorkspaceFacadeTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Keboola\JobQueue\JobConfiguration\Tests\Mapping\DataLoader;
+namespace Keboola\JobQueue\JobConfiguration\Tests\Mapping\StagingWorkspace;
 
 use Keboola\JobQueue\JobConfiguration\Mapping\StagingWorkspace\StagingWorkspaceFacade;
 use Keboola\StagingProvider\Workspace\WorkspaceProvider;
